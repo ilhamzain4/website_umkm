2a6943c4ca8de71651177588b7f371efc0cd956d
Safnaa26 <safnatur.rohmah25004@mhs.uingusdur.ac.id>
2026-09-13 14:31:14 +0700

    feat: buat landing page awal UMKM

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const promoButton = document.querySelector("#promoButton");
+
+promoButton.addEventListener("click", () => {
+    promoButton.textContent = "promo: Beli 2 gratis es teh jumbo!";
+    console.log("promo Kopi Inz berhasil ditampilkan.");
+});
